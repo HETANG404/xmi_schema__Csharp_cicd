03797017dbc40997713716a2880d23513f73f400
HETANG404 <hetang401@gmail.com>
2025-05-02 12:11:15 +0800

    update json form

--- Extensions/ExtensionNativeJsonBuilder.cs
+++ Extensions/ExtensionNativeJsonBuilder.cs
@@ -1,82 +1,81 @@
-namespace XmiCore;
-using Newtonsoft.Json;
-
-public class ExtensionNativeJsonBuilder
+namespace XmiCore
 {
-    private readonly XmiModel _model;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using Newtonsoft.Json;
 
-    public ExtensionNativeJsonBuilder(XmiModel model)
+    public class ExtensionNativeJsonBuilder
     {
-        _model = model;
-    }
+        private readonly XmiModel _model;
 
-    public string BuildJson()
-    {
-        var nodes = _model.Entities.Select(e => new
+        public ExtensionNativeJsonBuilder(XmiModel model)
         {
-            id = e.ID,
-            label = e.Name,
-            type = e.EntityType,
-            attributes = GetAttributes(e)
-        }).ToList();
-
-        var edges = _model.Relationships.Select(r => new
-        {
-            id = r.ID,
-            label = r.EntityType,
-            source = r.Source?.ID,
-            target = r.Target?.ID,
-            attributes = GetAttributes(r)
-        }).ToList();
-
-        var graphJson = new
-        {
-            nodes,
-            edges
-        };
-
-        return JsonConvert.SerializeObject(graphJson, Formatting.Indented);
-    }
-
-    public void Save(string path)
-    {
-        File.WriteAllText(path, BuildJson());
-        Console.WriteLine($"JSON 图文件保存成功：{path}");
-    }
-
-    private Dictionary<string, object> GetAttributes(object obj)
-    {
-        var dict = new Dictionary<string, object>();
-        var props = obj.GetType().GetProperties();
-
-        foreach (var prop in props)
-        {
-            var value = prop.GetValue(obj);
-            if (value == null)
-                continue;
-
-            var type = prop.PropertyType;
-
-            // 基础类型 + 字符串 + 枚举
-            if (type.IsPrimitive || type == typeof(string) || type.IsEnum || 
-                type == typeof(decimal) || type == typeof(DateTime) ||
-                type == typeof(float) || type == typeof(double))
-            {
-                dict[prop.Name] = value;
-            }
-            // 如果是单个 XmiBaseEntity 派生类 -> 取 ID
-            else if (value is XmiBaseEntity entityRef)
-            {
-                dict[prop.Name] = entityRef.ID;
-            }
-            // 如果是 IEnumerable<XmiBaseEntity> 类型（比如 List<XmiPoint>）
-            else if (value is IEnumerable<XmiBaseEntity> entityList)
-            {
-                dict[prop.Name] = entityList.Select(e => e.ID).ToList();
-            }
-            // 其他复杂类型跳过（避免嵌套）
+            _model = model;
         }
 
-        return dict;
+        public string BuildJson()
+        {
+            var nodes = _model.Entities.Select(e => new Dictionary<string, object>
+            {
+                [e.GetType().Name] = GetAttributes(e)
+            }).ToList();
+
+            var edges = _model.Relationships.Select(r => new Dictionary<string, object>
+            {
+                [r.GetType().Name] = GetAttributes(r)
+            }).ToList();
+
+            var graphJson = new
+            {
+                nodes,
+                edges
+            };
+
+            return JsonConvert.SerializeObject(graphJson, Formatting.Indented);
+        }
+
+        public void Save(string path)
+        {
+            File.WriteAllText(path, BuildJson());
+            Console.WriteLine($"JSON 图文件保存成功：{path}");
+        }
+
+        private Dictionary<string, object> GetAttributes(object obj)
+        {
+            var dict = new Dictionary<string, object>();
+
+            var props = obj.GetType()
+                .GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                .OrderBy(p => p.MetadataToken); // 声明顺序排序
+
+            foreach (var prop in props)
+            {
+                var value = prop.GetValue(obj);
+                if (value == null)
+                    continue;
+
+                var type = prop.PropertyType;
+
+                if (type.IsPrimitive || type == typeof(string) || type.IsEnum ||
+                    type == typeof(decimal) || type == typeof(DateTime) ||
+                    type == typeof(float) || type == typeof(double))
+                {
+                    dict[prop.Name] = value;
+                }
+                else if (value is XmiBaseEntity entityRef)
+                {
+                    dict[prop.Name] = entityRef.ID;
+                }
+                else if (value is IEnumerable<XmiBaseEntity> entityList)
+                {
+                    dict[prop.Name] = entityList.Select(e => e.ID).ToList();
+                }
+            }
+
+            return dict;
+        }
     }
 }
